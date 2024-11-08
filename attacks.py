cdce18fdf2c4a94a6d88f91ebd32d4750290a686
Manuel <manuelguvi500@gmail.com>
2024-11-08 00:22:16 -0500

    Se agregó el codigo flotante a la clase AttackManager

--- attacks.py
+++ attacks.py
@@ -193,111 +193,113 @@ class PhishingAttack(Attack):
         self.driver.quit()
         print("Ataque de phishing detenido.")
 
-def authenticate_user():
-    user_manager = UserManager()
-    user_manager.load_users('users.txt')
-    while True:
-        print("1. Registrarse")
-        print("2. Iniciar sesión")
-        print("3. Salir")
-        choice = input("Elige una opción: ")
-        if choice == '1':
-            username = input("Introduce el nombre de usuario: ")
-            role = input("Introduce el rol (admin/user): ").upper()
-            if role in Role.__members__:
-                user = user_manager.add_user(username, Role[role])
-                user_manager.save_users('users.txt')
-                print("¡Usuario registrado con éxito!")
-                return user
+class AttackManager:
+    def authenticate_user(self):
+        user_manager = UserManager()
+        user_manager.load_users('users.txt')
+        while True:
+            print("1. Registrarse")
+            print("2. Iniciar sesión")
+            print("3. Salir")
+            choice = input("Elige una opción: ")
+            if choice == '1':
+                username = input("Introduce el nombre de usuario: ")
+                role = input("Introduce el rol (admin/user): ").upper()
+                if role in Role.__members__:
+                    user = user_manager.add_user(username, Role[role])
+                    user_manager.save_users('users.txt')
+                    print("¡Usuario registrado con éxito!")
+                    return user
+                else:
+                    print("Rol inválido. Por favor, inténtalo de nuevo.")
+            elif choice == '2':
+                username = input("Introduce el nombre de usuario: ")
+                user = user_manager.get_user(username)
+                if user:
+                    print(f"¡Bienvenido {user.username}!")
+                    return user
+                else:
+                    print("Usuario no encontrado. Por favor, regístrate primero.")
+            elif choice == '3':
+                return None
             else:
-                print("Rol inválido. Por favor, inténtalo de nuevo.")
-        elif choice == '2':
-            username = input("Introduce el nombre de usuario: ")
-            user = user_manager.get_user(username)
-            if user:
-                print(f"¡Bienvenido {user.username}!")
-                return user
+                print("Opción inválida. Por favor, inténtalo de nuevo.")
+
+    def initDDoS(self, user) -> None:
+        target_url = input("Ingrese la URL de destino para el ataque DDoS (default: http://testphp.vulnweb.com): ") or "http://testphp.vulnweb.com"
+        n_ips = input("Ingrese el número de IPs (default: 10): ") or 10
+        n_msg = input("Ingrese el número de mensajes por IP (default: 5): ") or 5
+        threads = input("Ingrese el número de hilos (default: 4): ") or 4
+        ddos_attack = DDoSAttack(target_url, int(n_ips), int(n_msg), int(threads))
+        save_attack_state(ddos_attack, user.username)
+        ddos_attack.start()
+        save_attack_state(ddos_attack, user.username)
+
+    def initSQL(self, user) -> None:
+        target_url = input("Ingrese la URL para el ataque SQL Injection (default: generic url): ") or "http:testphp.vulnweb.com/showimage.php?file=1"
+        payload = input('Ingrese el payload (default: OR 1=1): ') or 'OR 1=1'
+        sql_injection_attack = SQLInjectionAttack(target_url, payload)
+        save_attack_state(sql_injection_attack, user.username)
+        sql_injection_attack.start()
+        save_attack_state(sql_injection_attack, user.username)
+
+    def initPhishing(self, user) -> None:
+        target_emails = input("Ingrese los correos electrónicos de destino separados por comas (default: correos predefinidos): ") or "manelguvi100@gmail.com,manelguvi200@gmail.com"
+        template = input("Ingrese la plantilla del correo de phishing (default: Este es un correo de phishing. Por favor, haga clic en el enlace malvado D): ") or "Este es un correo de phishing. Por favor, haga clic en el enlace malvado D:"
+        phishing_attack = PhishingAttack(target_emails.split(','), template)
+        save_attack_state(phishing_attack, user.username)
+        phishing_attack.start()
+        save_attack_state(phishing_attack, user.username)
+
+    def main_menu(self, user):
+        while True:
+            print("\nSeleccione el tipo de ataque a ejecutar:")
+            print("1. DDoS Attack")
+            print("2. SQL Injection Attack")
+            print("3. Phishing Attack")
+            print("4. Ver informes de ataque")
+            print("5. Salir")
+            choice = input("Ingrese el número de su elección: ")
+            if choice == '1':
+                self.initDDoS(user)
+            elif choice == '2':
+                self.initSQL(user)
+            elif choice == '3':
+                self.initPhishing(user)
+            elif choice == '4':
+                self.view_attack_reports(user.username)
+            elif choice == '5':
+                print("Saliendo...")
+                break
             else:
-                print("Usuario no encontrado. Por favor, regístrate primero.")
-        elif choice == '3':
-            return None
+                print("Opción no válida. Por favor, intente de nuevo.")
+
+    def view_attack_reports(self, username):
+        reports_dir = 'ataques'
+        if not os.path.exists(reports_dir):
+            print("No hay informes de ataque disponibles.")
+            return
+
+        user_reports = [f for f in os.listdir(reports_dir) if f.startswith(username + "_")]
+        if not user_reports:
+            print("No hay informes de ataque disponibles para tu usuario.")
+            return
+
+        print("\nInformes de ataque disponibles:")
+        for report in user_reports:
+            print(report)
+        report_choice = input("Ingrese el nombre del informe que desea ver: ")
+        if report_choice in user_reports:
+            with open(os.path.join(reports_dir, report_choice), 'r') as file:
+                print(file.read())
         else:
-            print("Opción inválida. Por favor, inténtalo de nuevo.")
-
-def initDDoS() -> None:
-    target_url = input("Ingrese la URL de destino para el ataque DDoS (default: http://testphp.vulnweb.com): ") or "http://testphp.vulnweb.com"
-    n_ips = input("Ingrese el número de IPs (default: 10): ") or 10
-    n_msg = input("Ingrese el número de mensajes por IP (default: 5): ") or 5
-    threads = input("Ingrese el número de hilos (default: 4): ") or 4
-    ddos_attack = DDoSAttack(target_url, int(n_ips), int(n_msg), int(threads))
-    save_attack_state(ddos_attack, user.username)
-    ddos_attack.start()
-    save_attack_state(ddos_attack, user.username)
-def initSQL() -> None:
-    target_url = input("Ingrese la URL para el ataque SQL Injection (default: generic url): ") or "http:testphp.vulnweb.com/showimage.php?file=1"
-    payload = input('Ingrese el payload (default: OR 1=1): ') or 'OR 1=1'
-    sql_injection_attack = SQLInjectionAttack(target_url, payload)
-    save_attack_state(sql_injection_attack, user.username)
-    sql_injection_attack.start()
-    save_attack_state(sql_injection_attack, user.username)
-def initPhishing() -> None:
-    target_emails = input("Ingrese los correos electrónicos de destino separados por comas (default: correos predefinidos): ") or "manelguvi100@gmail.com,manelguvi200@gmail.com"
-    template = input("Ingrese la plantilla del correo de phishing (default: Este es un correo de phishing. Por favor, haga clic en el enlace malvado D): ") or "Este es un correo de phishing. Por favor, haga clic en el enlace malvado D:"
-    phishing_attack = PhishingAttack(target_emails.split(','), template)
-    save_attack_state(phishing_attack, user.username)
-    phishing_attack.start()
-    save_attack_state(phishing_attack, user.username)
-
-def main_menu(user):
-    while True:
-        print("\nSeleccione el tipo de ataque a ejecutar:")
-        print("1. DDoS Attack")
-        print("2. SQL Injection Attack")
-        print("3. Phishing Attack")
-        print("4. Ver informes de ataque")
-        print("5. Salir")
-        choice = input("Ingrese el número de su elección: ")
-        if choice == '1':
-            initDDoS()
-        elif choice == '2':
-            initSQL()
-        elif choice == '3':
-            initPhishing()
-        elif choice == '4':
-            view_attack_reports(user.username)
-        elif choice == '5':
-            print("Saliendo...")
-            break
-        else:
-            print("Opción no válida. Por favor, intente de nuevo.")
-
-        def view_attack_reports(username):
-            reports_dir = 'ataques'
-            if not os.path.exists(reports_dir):
-                print("No hay informes de ataque disponibles.")
-                return
-
-            user_reports = [f for f in os.listdir(reports_dir) if f.startswith(username + "_")]
-            if not user_reports:
-                print("No hay informes de ataque disponibles para tu usuario.")
-                return
-
-            print("\nInformes de ataque disponibles:")
-            for report in user_reports:
-                print(report)
-            report_choice = input("Ingrese el nombre del informe que desea ver: ")
-            if report_choice in user_reports:
-                with open(os.path.join(reports_dir, report_choice), 'r') as file:
-                    print(file.read())
-
-            else:
-                print("Informe no encontrado.")
+            print("Informe no encontrado.")
 
 
 if __name__ == '__main__':
     from users import User, Role, UserManager
     from attackstatemng import save_attack_state
     import os
-    user = authenticate_user()
-    main_menu(user)
-
+    attack_manager = AttackManager()
+    user = attack_manager.authenticate_user()
+    attack_manager.main_menu(user)
